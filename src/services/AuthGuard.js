823d3c974bfe40b6b20327e01f22acc00c7de6d6
haythamdahri <haytham.dahri@gmail.com>
2020-07-14 10:17:35 +0100

    Initial commit

---
+++ src/services/AuthGuard.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import Unauthorized from "../components/Unauthorized";
+import AuthService from "./AuthService";
+
+export default function PrivateRoute({ children, ...props }) {
+  return (
+    <Route
+      {...props}
+      render={({ location }) =>
+        AuthService.isAuthorized() ? (
+          children
+        ) : AuthService.isAuthenticated() ? (
+          <Unauthorized />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/signin",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+export function UserRoute({ children, ...props }) {
+  return (
+    <Route
+      {...props}
+      render={({ location }) =>
+        AuthService.isAuthenticated() ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/signin",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+export function AuthenticatedGuard({ children, ...props }) {
+  return (
+    <Route
+      {...props}
+      render={({ location }) =>
+        !AuthService.isAuthenticated() ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
---
+++ src/services/AuthService.js
@@ -0,0 +1,85 @@
+import axios from "axios";
+import UserTokenModel from "../models/UserTokenModel";
+
+const API_URL = "http://localhost:8080/auth/";
+const STORAGE_USER = "user";
+
+class AuthService {
+  signin(user) {
+    return axios
+      .post(API_URL, user)
+      .then((userData) => {
+        let userToken = this.decodeToken(userData.data.token);
+        localStorage.setItem(STORAGE_USER, JSON.stringify(userToken));
+        return userToken;
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  }
+
+  signout(history = null) {
+    localStorage.removeItem(STORAGE_USER);
+    if (history != null) {
+      history.push("/signin");
+    }
+  }
+
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem(STORAGE_USER) || "{}");
+  }
+
+  isAuthenticated() {
+    return localStorage.getItem(STORAGE_USER) != null;
+  }
+
+  isAuthorized() {
+    return this.isAuthenticated() && (this.isAdmin() || this.isEmployee());
+  }
+
+  // Check if user has role
+  async hasRole(roleName) {
+    let userToken = JSON.parse(
+      localStorage.getItem(STORAGE_USER) || "{}"
+    );
+    return (
+      userToken?.roles?.find((role) => role.authority === roleName) != null
+    );
+  }
+
+  // Check if connected user is an admin
+  isAdmin() {
+    let userToken = JSON.parse(
+      localStorage.getItem(STORAGE_USER) || "{}"
+    );
+    return (
+      userToken?.roles?.find((role) => role.authority === "ROLE_ADMIN") != null
+    );
+  }
+
+  // Check if connected user is an admin
+  isEmployee() {
+    let userToken = JSON.parse(
+      localStorage.getItem(STORAGE_USER) || "{}"
+    );
+    return (
+      userToken?.roles?.find((role) => role.authority === "ROLE_EMPLOYEE") !=
+      null
+    );
+  }
+
+  // Decode token
+  decodeToken(token) {
+    var jwtDecode = require("jwt-decode");
+    const decoded = jwtDecode(token);
+    let userToken = new UserTokenModel();
+    userToken.bearerToken = "Bearer " + token;
+    userToken.token = token;
+    userToken.email = decoded.sub;
+    userToken.roles = decoded.roles;
+    userToken.exp = Number(decoded.exp * 1000);
+    return userToken;
+  }
+}
+
+export default new AuthService();
